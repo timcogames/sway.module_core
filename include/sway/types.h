fca07cf7de40bc4a27330a9d92742b2e712effa8
Victor Timoshin <victor-timoshin@hotmail.com>
2018-07-02 19:57:57 +0300

    Добавлен перечислитель фундаментальных типов

--- include/sway/types.h
+++ include/sway/types.h
@@ -5,7 +5,20 @@
 
 NAMESPACE_BEGIN(sway)
 
-// signed
+enum Types {
+	kType_None, /*!< Неопределенный тип */
+	kType_Char, /*!< Тип 8bit целого числа со знаком */
+	kType_Short, /*!< Тип 16bit целого числа со знаком */
+	kType_Int, /*!< Тип 32bit целого числа со знаком */
+	kType_Long, /*!< Тип 64bit целого числа со знаком */
+	kType_UChar, /*!< Тип 8bit целого числа без знака */
+	kType_UShort, /*!< Тип 16bit целого числа без знака */
+	kType_UInt, /*!< Тип 32bit целого числа без знака */
+	kType_ULong, /*!< Тип 64bit целого числа без знака */
+	kType_Float,
+	kType_Double
+};
+
 typedef char s8_t;
 typedef short s16_t;
 typedef int s32_t;
@@ -13,13 +26,11 @@ typedef float f32_t;
 typedef long s64_t;
 typedef double f64_t;
 
-// unsigned
 typedef unsigned char u8_t;
 typedef unsigned short u16_t;
 typedef unsigned int u32_t;
 typedef unsigned long u64_t;
 
-// simplified
 typedef s8_t * lpstr_t;
 typedef const s8_t * lpcstr_t;
 
