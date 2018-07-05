69f6e364183526f6c29e847dd79d9a9c3f44dd22
Victor Timoshin <victor-timoshin@hotmail.com>
2018-07-05 11:22:55 +0300

    Перенесён класс filesource в sway/core/generic/io

---
+++ include/sway/core/generic/io/filesource.h
@@ -0,0 +1,50 @@
+#ifndef SWAY_CORE_GENERIC_IO_FILESOURCE_H
+#define SWAY_CORE_GENERIC_IO_FILESOURCE_H
+
+#include <sway/namespacemacros.h>
+#include <fstream> // std::ifstream
+
+NAMESPACE_BEGIN(sway)
+NAMESPACE_BEGIN(core)
+NAMESPACE_BEGIN(generic)
+NAMESPACE_BEGIN(io)
+
+class FileSource {
+public:
+	/*!
+	 * \brief
+	 *    Конструктор класса по умолчанию.
+	 */
+	FileSource();
+
+	/*!
+	 * \brief
+	 *    Деструктор класса.
+	 */
+	virtual ~FileSource() = default;
+
+	/*!
+	 * \brief
+	 *    Последовательное чтение файла.
+	 * 
+	 * \param[in] source
+	 *    Ссылка на поток.
+	 */
+	virtual void * loadFromStream(std::ifstream & source) = 0;
+
+	/*!
+	 * \brief
+	 *    Возвращает, открыт ли файл.
+	 */
+	bool isFileOpened() const;
+
+protected:
+	bool _fileOpened; /*!< Открыт ли файл в данный момент. */
+};
+
+NAMESPACE_END(io)
+NAMESPACE_END(generic)
+NAMESPACE_END(core)
+NAMESPACE_END(sway)
+
+#endif // SWAY_CORE_GENERIC_IO_FILESOURCE_H
---
+++ src/generic/io/filesource.cpp
@@ -0,0 +1,20 @@
+#include <sway/core/generic/io/filesource.h>
+
+NAMESPACE_BEGIN(sway)
+NAMESPACE_BEGIN(core)
+NAMESPACE_BEGIN(generic)
+NAMESPACE_BEGIN(io)
+
+FileSource::FileSource()
+	: _fileOpened(false) {
+	// Empty
+}
+
+bool FileSource::isFileOpened() const {
+	return _fileOpened;
+}
+
+NAMESPACE_END(io)
+NAMESPACE_END(generic)
+NAMESPACE_END(core)
+NAMESPACE_END(sway)
