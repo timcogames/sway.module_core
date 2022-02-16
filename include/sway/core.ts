9ee7c66beaecd7f61a5e8e12eaec3560e2594fff
apriori85 <victor-timoshin@hotmail.com>
2022-02-16 18:04:38 +0300

    Changed option used compiler, added IObjectWrap

--- include/sway/core.ts
+++ include/sway/core.ts
@@ -6,6 +6,12 @@ import { Node } from './core/container/node';
 export type INodeIdx = NodeIdx.BaseIface;
 export type INode = Node.BaseIface<{}>;
 
+interface IObjectWrap {
+    __parent: any;
+    __construct(args?: any): void;
+    __destruct(): void;
+};
+
 export interface ICoreSubmodule {
     NodeIdx: NodeIdx.EmscriptenClass;
     Node: Node.EmscriptenClass;
