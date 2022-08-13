1bfa6fa40e4f0ad9f0408f3c2a2b604e8542c663
apriori85 <victor-timoshin@hotmail.com>
2022-08-13 13:27:08 +0300

    Refactoring

--- lib/include/sway/emscriptenmacros.hpp
+++ lib/include/sway/emscriptenmacros.hpp
@@ -1,9 +1,11 @@
 #ifndef SWAY_EMSCRIPTENMACROS_HPP
 #define SWAY_EMSCRIPTENMACROS_HPP
 
-#define DECLARE_EMSCRIPTEN_BINDING() static void bindEmscriptenClass();
+#define DECLARE_EMSCRIPTEN_BINDING() \
+public:                              \
+  static void bindEmscriptenClass();
 
-#define EMSCRIPTEN_BINDING_BEGIN(cls) void cls::bindEmscriptenClass() {
+#define EMSCRIPTEN_BINDING_BEGIN(objclass) void objclass::bindEmscriptenClass() {
 
 #define EMSCRIPTEN_BINDING_END() }
 
