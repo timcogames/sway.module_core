73e6dcf8027bcd4a27e4193caf48e688951acd10
apriori85 <victor-timoshin@hotmail.com>
2023-10-11 09:23:30 +0300

    Added remove bitcode command line

--- cmakehelpers/create_library.cmake
+++ cmakehelpers/create_library.cmake
@@ -7,9 +7,13 @@ function(create_library #[[TARGET]] target_arg #[[TYPE]] type_arg #[[SOURCE_DIR]
     message(FATAL_ERROR "Invalid library type")
   endif()
 
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=default -fPIE" PARENT_SCOPE)
+
   file(GLOB_RECURSE SOURCE_FILE_LIST ${source_dir_arg})
 
   add_library(${target_arg} ${type_arg} ${SOURCE_FILE_LIST})
+  # set_target_properties(${META_PRSN_LIBNAME} PROPERTIES LINK_FLAGS "-static -no-pie")
+  set_target_properties(${target_arg} PROPERTIES LINK_FLAGS "-Wall")
   set_target_properties(${target_arg} PROPERTIES OUTPUT_NAME "module_${target_arg}")
   set_target_properties(${target_arg} PROPERTIES SUFFIX ${LIB_FILE_EXTENSION})
 
@@ -23,4 +27,5 @@ function(create_bitcode_library #[[TARGET]] target_arg #[[SOURCE_DIR]] source_di
   endif()
 
   create_library(${target_arg} STATIC ${source_dir_arg})
+  set_target_properties(${META_PRSN_LIBNAME} PROPERTIES LINK_FLAGS "--no-entry")
 endfunction(create_bitcode_library)
--- cmakehelpers/project_metadata.cmake
+++ cmakehelpers/project_metadata.cmake
@@ -2,6 +2,7 @@ function(project_metadata #[[PROJ_NAME]] proj_name_arg #[[VERSION]] version_arg)
   string(TOUPPER ${proj_name_arg} proj_name_upper)
 
   set(MODULE_${proj_name_upper}_PROJ_NAME "${proj_name_arg}" PARENT_SCOPE)
+  set(MODULE_${proj_name_upper}_TARGET "module_${proj_name_arg}" PARENT_SCOPE)
   set(MODULE_${proj_name_upper}_VERSION "${version_arg}" PARENT_SCOPE)
   set(MODULE_${proj_name_upper}_LIB_NAME "libmodule_${proj_name_arg}" PARENT_SCOPE)
   set(MODULE_${proj_name_upper}_LIB_NAME_VER "libmodule_${proj_name_arg}.${version_arg}" PARENT_SCOPE)
