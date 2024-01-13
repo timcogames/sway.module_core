0c8fe4c0ef69b4a73af3f4f5bb719714f0bd78f1
apriori85 <victor-timoshin@hotmail.com>
2024-01-13 23:58:40 +0300

    Added check MODULE_CORE_OBJ_TARGET exists

--- cmake_modules/add_submodule.cmake
+++ cmake_modules/add_submodule.cmake
@@ -1,6 +1,16 @@
-function(add_submodule #[[ARG1]] name_arg 
-                       #[[ARG2]] source_dir_arg)
-  if(NOT TARGET ${name_arg})
-    add_subdirectory(${source_dir_arg})
+#--------------------------------------------------------------------------------
+#[[
+# DESC
+  Add submodule
+
+# ARGS
+  ARG1:STRING name [IN] - 
+  ARG2:STRING module_dir [IN] - 
+ ]]
+#--------------------------------------------------------------------------------
+function(add_submodule #[[ARG1]] name
+                       #[[ARG2]] module_dir)
+  if(NOT TARGET ${name})
+    add_subdirectory(${module_dir})
   endif()
 endfunction(add_submodule)
