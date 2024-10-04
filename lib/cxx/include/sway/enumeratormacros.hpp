88dc3c3bca37404ffc63e147cb7a2569d9d5dd73
apriori85 <victor-timoshin@hotmail.com>
2024-10-04 19:46:10 +0300

    Added ENUM_DEF_BEGIN, ENUM_IDX_BEGIN

--- lib/cxx/include/sway/enumeratormacros.hpp
+++ lib/cxx/include/sway/enumeratormacros.hpp
@@ -5,18 +5,22 @@
 
 NAMESPACE_BEGIN(sway)
 
-#define ENUM_MAX_ELEMENTS(NAME) core::detail::toBase(NAME::Enum::Latest)
-
 // clang-format off
-#define ENUM_BEGIN(NAME, TYPE) \
-  namespace NAME {             \
-    enum class Enum : TYPE {
+#define ENUM_BEGIN(NAME, TYPE, INITIAL_VALUE) \
+  namespace NAME {                            \
+    enum class Enum : TYPE {                  \
+      NONE = INITIAL_VALUE,
+
+#define ENUM_DEF_BEGIN(NAME) ENUM_BEGIN(NAME, u32_t, 0)
+#define ENUM_IDX_BEGIN(NAME) ENUM_BEGIN(NAME, i32_t, GLOB_IDX_INVALID)
 
 #define ENUM_END()             \
       , Latest                 \
     };                         \
   }
 // clang-format on
+
+#define ENUM_MAX_ELEMENTS(NAME) core::detail::toBase(NAME::Enum::Latest)
 
 NAMESPACE_END(sway)
 
